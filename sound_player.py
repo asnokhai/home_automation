f72f8b84e4e393fe4f47bfb36e3638eb9c694803
asnokhai <asnokhai@tudelft.nl>
2026-07-16 21:22:10 +0200

    Using a .wav sound file instead now

--- sound_player.py
+++ sound_player.py
@@ -3,7 +3,7 @@ import pygame
 class SoundPlayer:
     def __init__(self):
         pygame.mixer.init()
-        self._sound = pygame.mixer.Sound('button-click.mp3')
+        self._sound = pygame.mixer.Sound('button-click.wav')
 
     def play(self):
         self._sound.play()
